6975dfdbd7f6223bc4df32d7abbb1ac8e578ffde
greatgabbar <strivedi_be19@thapar.edu>
2022-02-18 11:39:27 +0530

    DONE : Navigation bar

---
+++ pages/news/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const News: NextPage = () => {
+  return <div className={styles.container}>News</div>;
+};
+
+export default News;
